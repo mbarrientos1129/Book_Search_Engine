0c212ea90ebd7113bf8db730dd778544ab0a9d29
mbarrientos.webdevelopment@gmail.com <91514897+mbarrientos1129@users.noreply.github.com>
2022-05-09 18:46:04 -0400

    third heroku commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require("./schema")
+const { typeDefs, resolvers } = require("./Schema")
 const { authMiddleware } = require('./utils/auth')
 
 const app = express();
